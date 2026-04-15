f24c554c2aa9516b56e49652b17fe13e1b7c88b8
Abhixshek7 <abhixshek20@gmail.com>
2026-04-15 14:07:44 +0530

    Fixed Auth Page

--- frontend/src/pages/auth/LoginPage.tsx
+++ frontend/src/pages/auth/LoginPage.tsx
@@ -27,7 +27,7 @@ export default function LoginPage() {
   });
 
   const handleGoogleLogin = () => {
-    window.location.href = "http://localhost:5000/api/auth/google";
+    window.location.href = "https://invt-nzwm.onrender.com/api/auth/google/callback";
   };
 
   // Check for token or error in URL on load
